2ef1385212ad3f076def071a10b34580d8aa0721
mac <mac@esco-3.local>
2022-01-31 15:33:32 +0000

    added side navigation

--- js/utils.js
+++ js/utils.js
@@ -17,3 +17,23 @@ async function getItem(category, name) {
   // find item
   return data.find((item) => item.name === name);
 }
+
+// handle navigation
+function handleNavigation() {
+  const menu = document.querySelector(".menu");
+  const close = document.querySelector(".close");
+  const links = document.querySelector(".links");
+
+  menu.addEventListener("click", function () {
+    links.classList.add("active");
+  });
+
+  close.addEventListener("click", function () {
+    links.classList.remove("active");
+  });
+}
+function inti() {
+  handleNavigation();
+}
+
+inti();
