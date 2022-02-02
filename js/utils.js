c964a64b510789911e85d3d9bec514c5b4510d5f
mac <mac@esco-3.local>
2022-02-02 09:13:15 +0000

    update fixed bug with store data

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,15 @@
+// get store data
+async function getStoreData(category = "hoodies") {
+  const res = await fetch("../../data/data.json");
+  const data = await res.json();
+
+  const dataToFormat = data[category];
+
+  return dataToFormat.map((item) => {
+    return { ...item, category };
+  });
+}
+
 // handle navigation
 function handleNavigation() {
   const menu = document.querySelector(".menu");
